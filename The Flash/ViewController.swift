d7b168adcfa160a328068f56ca80fc1fd8fee4ae
user_19 <user_19@mac-19deimac.local>
2017-03-29 13:38:02 +0800

    Initial Commit

---
+++ The Flash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Flash
+//
+//  Created by user_19 on 2017/3/29.
+//  Copyright © 2017年 g0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
